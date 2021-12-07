b1b45bb739c93c9e760a93c23cdfba832931312d
Finn <32446445+Delafinn@users.noreply.github.com>
2021-12-06 17:16:50 -0700

    Day3 project 1.1
    updated project to change the inputs to lowercase.

--- TreasureIslandgame.py
+++ TreasureIslandgame.py
@@ -23,15 +23,15 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 while True:
-  first_question = input('Youre at a crossroad. Where do you want to go? Type "left" or "right"')
+  first_question = input('Youre at a crossroad. Where do you want to go? Type "left" or "right"').lower()
   if first_question == "right" or first_question == "RIGHT" or first_question == "Right":
     print("you have fallen into a hole.. Game over!")
   if first_question == "left" or first_question == "LEFT" or first_question == "Left":
-    second_question = input('Youve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.')
+    second_question = input('Youve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.').lower()
     if second_question == "swim" or second_question == "SWIM" or second_question == "Swim":
       print('you were eaten by the lake monster.. Game over!')
     if second_question == "wait" or second_question == "WAIT" or second_question == "Wait":
-      third_question = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?")
+      third_question = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?").lower()
       if third_question == "blue" or third_question == "red"  or third_question == "Red"  or third_question == "Blue":
         ("Game over!")
       if third_question == "yellow" or third_question == "Yellow":
