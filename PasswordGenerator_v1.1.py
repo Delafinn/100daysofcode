4e98e9b2b17e91cc6a0b4907fcb3ed354c7b40d8
Finn <32446445+Delafinn@users.noreply.github.com>
2021-12-18 12:26:39 -0700

    PasswordGenerator_v1.2
    updated Day5 project.

--- PasswordGenerator_v1.1.py
+++ PasswordGenerator_v1.1.py
@@ -5,26 +5,30 @@ capletters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', '
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '$', '%', '&','*', '+']
 
-fulllist = letters + numbers + symbols +capletters
+fulllist = letters + numbers + symbols + capletters
 
 #lists above contain characters used in passwords
 
 
 print("Welcome to quick password generator")
+run_program = True
 
-n_characters = int(input("How many characters would you like in your password?"))
+while run_program == True:
+    proceed_question = input("would you like to proceed?").lower()
+    if proceed_question == "no" or proceed_question == "n":
+        run_program == False
+        quit()
 
+    elif proceed_question == "yes" or proceed_question == "y":
+        n_characters = int(input("How many characters would you like in your password? Please enter a number."))
+        passwordlist = []  #starting with en empty list
+        for i in range(0, n_characters):
+            passwordlist.append(random.choice(fulllist)) #adding to the empty passwordlist randomly from fulllist
+        random.shuffle(passwordlist) #shuffling the passwordlist
+        password = "" #now we make an empty string
+        # we are making a string so the generated password is readable to the human eyes. and not in a list separated by commas.
 
-passwordlist = []  #starting with en empty list
+        for items in passwordlist: # from i or items in passwordlist
+            password += items # add i or items to the string password
 
-for i in range(0, n_characters): # telling the computer how many characters we are gonna loop for
-  passwordlist.append(random.choice(fulllist)) #adding to the empty passwordlist randomly from fulllist
-
-random.shuffle(passwordlist) #shuffling the passwordlist
-
-password = "" #now we make an empty string
-
-for items in passwordlist: # from i or items in passwordlist
-  password += items # add i or items to the string password
-
-print(password) #print password
+        print(password) #print password
