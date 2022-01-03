e6f48dc2376052bfe51db0707154f279fad13d94
Finn <32446445+Delafinn@users.noreply.github.com>
2022-01-02 21:32:05 -0700

    Updated Day12 project
    fixed bugs in the while loop.

--- numberguesser.py
+++ numberguesser.py
@@ -11,11 +11,15 @@ random_number = random.randint(1,101)
 
 print("Welcome to the number guessing game!")
 difficulty = input("what difficulty do you want? easy mode or hard mode?")
+
 if difficulty == "easy" or difficulty == "e":
     guesses += 6
+
 elif difficulty == "hard" or difficulty == "h":
     guesses  += 2
-print(f"you have {guesses} during this match!")
+
+print(f"you have {guesses} guesses during this match!")
+
 while guessed_number != random_number:
 
     guessed_number = input("I am thinking of a number. it is between one and one hundred. \n Guess the number!")
@@ -30,19 +34,26 @@ while guessed_number != random_number:
 
 
     elif guessed_number > random_number:
-        print("oof, the number you guessed was too high, guess again.")
+        print(f"oof, the number you guessed was {guessed_number} too high, guess again.")
         time.sleep(2)
         guesses -= 1
-        continue
+        if guesses == 0:
+            print(f"Oh no! you lose! \n the random number was : {random_number}")
+            break
+        else:
+            continue
 
     elif guessed_number < random_number:
-        print("oof, the number you guessed was too low, guess again.")
+        print(f"oof, the number you guessed was {guessed_number} too low, guess again.")
         time.sleep(2)
         guesses -= 1
-        continue
+        if guesses == 0:
+            print(f"Oh no! you lose! \n the random number was : {random_number}")
+            break
+        else:
+            continue
 
-if guesses == 0:
-    print(f"Oh no! you lose! \n the random number was : {random_number}")
+
 
 # If they got the answer correct, show the actual answer to the player.
 # Track the number of turns remaining.
