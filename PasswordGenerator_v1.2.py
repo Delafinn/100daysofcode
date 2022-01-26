cbcfb7493893a8c2bddd9ad34789ec811328420c
Finn <32446445+Delafinn@users.noreply.github.com>
2022-01-25 18:49:03 -0700

    Update PasswordGenerator_v1.2.py
    ran pylint several times for code fixes

--- PasswordGenerator_v1.2.py
+++ PasswordGenerator_v1.2.py
@@ -1,29 +1,32 @@
 #Password Generator Project
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
-capletters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+import sys
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+ 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
+capletters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+ 'M','N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '$', '%', '&','*', '+']
 
-fulllist = letters + numbers + symbols + capletters
+full_list = letters + numbers + symbols + capletters
 
 #lists above contain characters used in passwords
 
 
 print("Welcome to quick password generator")
-run_program = True
+RUN_PROGRAM = True
 
-while run_program == True:
+while RUN_PROGRAM is True:
     proceed_question = input("would you like to proceed?").lower()
     if proceed_question == "no" or proceed_question == "n":
-        run_program == False
-        quit()
+        sys.exit()
 
     elif proceed_question == "yes" or proceed_question == "y":
         n_characters = int(input("How many characters would you like in your password? Please enter a number."))
         passwordlist = []  #starting with en empty list
         for i in range(0, n_characters):
-            passwordlist.append(random.choice(fulllist)) #adding to the empty passwordlist randomly from fulllist
+            passwordlist.append(random.choice(full_list)) #adding to the empty passwordlist randomly from full_list
         random.shuffle(passwordlist) #shuffling the passwordlist
         password = "" #now we make an empty string
         # we are making a string so the generated password is readable to the human eyes. and not in a list separated by commas.
