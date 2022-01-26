11770192d8cd5fc4ff461bd7fc843a51df6ed714
Finn <32446445+Delafinn@users.noreply.github.com>
2022-01-25 19:06:12 -0700

    Update and rename rock-paper-scissorsgame.py to rock_paper_scissorsgame.py
    refactored code.

--- rock_paper_scissorsgame.py
+++ rock_paper_scissorsgame.py
@@ -1,3 +1,4 @@
+''' rock paper scissors game'''
 import random
 print('''
 8b      db      d8 ,adPPYYba, 8b,dPPYba,
@@ -6,48 +7,49 @@ print('''
   `8bd8'  `8bd8'   88,    ,88 88
     YP      YP     `"8bbdP"Y8 88
     ''')
-usermoveset = ["rock","paper","scissors","r","p","s"]
+user_move_set = ["rock","paper","scissors","r","p","s"]
 moveset = ["rock","paper","scissors"]
 while True:
     print("inputs can be either the full word or just the first letter.")
-    question1 = input("are you going to be using rock, paper, or scissors? type quit or stop when you want to quit.").lower()
+    question1 = input("""are you going to be using rock, paper, or scissors?
+    type quit or stop when you want to quit.""").lower()
 
-    if question1 == "quit" or question1 == "stop":
-            break
+    if question1 in ("quit","stop"):
+        break
 
-    if question1 == "rock" or question1 == "r":
+    if question1 in ("rock" , "r"):
         computer_choice = random.choice(moveset)
-        if computer_choice == "rock" or computer_choice == "r":
+        if computer_choice in ("rock" , "r"):
             print("the computer choose:" + computer_choice)
             print("It's a draw.")
-        elif computer_choice == "paper" or computer_choice == "p":
+        elif computer_choice in ("paper" , "p"):
             print("the computer choose:" + computer_choice)
             print("you lose!")
-        elif computer_choice == "scissors" or computer_choice == "s":
+        elif computer_choice in ("scissors" , "s"):
             print("the computer choose:" + computer_choice)
             print("you win!")
 
-    elif question1 == "paper" or question1 == "p":
+    elif question1 in ("paper" , "p"):
         computer_choice = random.choice(moveset)
-        if computer_choice == "rock" or computer_choice == "r":
+        if computer_choice in ("rock" , "r"):
             print("the computer choose:" + computer_choice)
             print("you win!")
-        elif computer_choice == "paper" or computer_choice == "p":
+        elif computer_choice in ("paper" , "p"):
             print("the computer choose:" + computer_choice)
             print("It's a tie!")
-        elif computer_choice == "scissors" or computer_choice == "s":
+        elif computer_choice in ("scissors" ,"s"):
             print("the computer choose:" + computer_choice)
             print("you lose!")
 
-    elif question1 == "scissors" or question1 == "s":
+    elif question1 in ("scissors" , "s"):
         computer_choice = random.choice(moveset)
-        if computer_choice == "rock" or computer_choice == "r":
+        if computer_choice in ("rock" , "r"):
             print("the computer choose:" + computer_choice)
             print("you lose!")
-        elif computer_choice == "paper" or computer_choice == "p":
+        elif computer_choice in ("paper" , "p"):
             print("the computer choose:" + computer_choice)
             print("You win!")
-        elif computer_choice == "scissors" or computer_choice == "s":
+        elif computer_choice in ("scissors" , "s"):
             print("the computer choose:" + computer_choice)
             print("it's a tie!!")
     else:
