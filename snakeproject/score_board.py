257684af31ef8913561ada57c73ab6bc7ad0a59c
Finn <32446445+Delafinn@users.noreply.github.com>
2022-02-08 23:25:12 -0700

    Update score_board.py
    refactored code to add a high score.

--- snakeproject/score_board.py
+++ snakeproject/score_board.py
@@ -6,14 +6,16 @@ class Score_Board(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
         self.hideturtle()
         self.goto(0, 320)
         self.color("white")
-        self.write(f"Score: {self.score} ", align = "center", font=("Arial", 15, "normal"))
+        self.write(f"Score: {self.score} high_score: {self.high_score}", align = "center", font=("Arial", 15, "normal"))
 
     def update_SB(self):
         self.clear()
-        self.write(f"Score: {self.score} " , align = "center", font=("Arial", 15, "normal"))
+        self.write(f"Score: {self.score} high_score: {self.high_score}" , align = "center", font=("Arial", 15, "normal"))
 
 
     def Increase_Score(self):
@@ -21,8 +23,10 @@ class Score_Board(Turtle):
         self.update_SB()
 
 
-
-    def Game_over(self):
-        self.goto(0,0)
-        self.color("white")
-        self.write(" Gameover!", align = "center", font=("Arial", 32, "normal"))
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode = "w") as file:
+                file.write(f" {self.high_score}")
+        self.score = 0
+        self.update_SB()
