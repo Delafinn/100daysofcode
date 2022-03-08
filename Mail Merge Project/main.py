2e06afc857ab9cebf4ad76ffc1eb52acac0bd7aa
Finn <32446445+Delafinn@users.noreply.github.com>
2022-03-07 22:17:34 -0700

    Small Project to send birthday invites

---
+++ Mail Merge Project/main.py
@@ -0,0 +1,21 @@
+#TODO: Create a letter using starting_letter.txt
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+PLACEHOLDER = "[name]"
+
+with open("./Input/Names/invited_names.txt") as names:
+    name_content = names.readlines()
+
+with open("./Input/Letters/starting_letter.txt") as letter:
+    letter_content = letter.read()
+    for name in name_content:
+        stripped_name = name.strip()
+        new_letter = letter_content.replace(PLACEHOLDER,stripped_name)
+        print(new_letter)
+        with open(f"./Output/ReadyToSend/new_letterfor{stripped_name}.txt", mode = "w") as file:
+            file.write(f" \n {new_letter}")
