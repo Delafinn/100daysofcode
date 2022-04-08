49cf7ed5746ba8f148792a516ba4a498ba15b571
Finn <32446445+Delafinn@users.noreply.github.com>
2022-04-08 12:35:48 -0600

    main.py
    Refactored code.

--- SMTPlib/main.py
+++ SMTPlib/main.py
@@ -1,10 +1,11 @@
+'''building a basic quote emailer.'''
 import smtplib
 import datetime as dt
 import random
 
 # creating the connection to email
-my_email = "youremailaddress@email.com"
-password1 = "yourpassword here"
+MY_EMAIL = "youremailaddress@email.com"
+PASSWORD1 = "yourpassword here"
 
 # pulling the date time for now
 now = dt.datetime.now()
@@ -24,17 +25,16 @@ print(date_of_birth)
 
 
 
-## TODO: open the quotes file as a dictionary
+
 
 with open("quotes.txt") as data:
     data_lines = data.readlines()
     rquote = random.choice(data_lines)
     print(rquote)
 
-## TODO: if the day is monday send a random quote from the quotes.txt
 if weekday == 1:
     with smtplib.SMTP("smtp.gmail.com",port = 587) as connection:
         connection.starttls()
-        connection.login(user = my_email, password = password1)
-        connection.sendmail(from_addr = my_email,to_addrs = "other_email@email.com", msg = f"Subject:Monday Quotes!\n\n {rquote}. ")
+        connection.login(user = MY_EMAIL, password = PASSWORD1)
+        connection.sendmail(from_addr = MY_EMAIL,to_addrs = "other_email@email.com", msg = f"Subject:Monday Quotes!\n\n {rquote}. ")
         connection.close()
