8d992dbeb94f78314db95a7de6142072dbe041cb
Finn <32446445+Delafinn@users.noreply.github.com>
2022-04-25 21:55:30 -0600

    Update howmanyweeksleft.py
    added a print() function to allow the user to see the output after entering their age

--- howmanyweeksleft.py
+++ howmanyweeksleft.py
@@ -15,6 +15,7 @@ z = totmonths - agenmonths
 
 print(f"You have " + str(x) + " days, " + str(y) + "weeks, and " + str(z) + " months left. until you are 90.")
 
+print("press any key to exit")
 
 
 # now ask the user their birthday to calculate the exact amount of days, weeks, months they have till 90.
